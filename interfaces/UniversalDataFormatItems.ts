0aca382148fb9986b34095e30b88e66cc30ed176
João Cunha <joaocunha1211@gmail.com>
2022-05-26 18:11:30 +0100

    Added UniversalDataFormatItems interface

--- interfaces/UniversalDataFormatItems.ts
+++ interfaces/UniversalDataFormatItems.ts
@@ -1,5 +1,20 @@
 interface UniversalDataFormatItems {
-
+    REF: string,
+    description_short: string,
+    description: string,
+    imgage: string,
+    pvp_1: number,
+    pvp_2?: number,
+    pvp_3?: number,
+    stock?: number,
+    bar_code: string,
+    min_sell?: number,
+    brand?: string,
+    weight?: number,
+    dimensions? :{
+        width: number,
+        height: number
+    }
 }
 
 export {
